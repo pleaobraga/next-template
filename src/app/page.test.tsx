8e5ff1b8dbb1bf249fceb2530a32028920ef5308
pleaobraga <pleao.braga@gmail.com>
2025-08-06 08:11:50 -0300

    first commit

---
+++ src/app/page.test.tsx
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Home from './page';
+
+describe('Home component', () => {
+  it('renders the heading and paragraph', () => {
+    render(<Home />);
+    
+    expect(screen.getByRole('heading', { name: /welcome to next\.js template/i })).toBeInTheDocument();
+    expect(screen.getByText(/this is a starter template for next\.js applications\./i)).toBeInTheDocument();
+  });
+});
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl font-bold">Welcome to Next.js Template</h1>
+        <p className="text-lg">
+          This is a starter template for Next.js applications.
+        </p>
+        </main>
+    </div>
+  );
+}
